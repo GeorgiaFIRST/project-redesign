6a4b331af1a27f30b21e529695e25ed0236e6b61
Natasha <natset456@gmail.com>
2021-02-27 22:22:13 -0500

    :lipstick: incorporated program area from frontend prototype, listing frc, ftc, lego league, and lego league jr

---
+++ components/programs.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import programs from "./programs.module.css";
+
+let Programs = () => {
+    return (
+        <div>
+<section className={`${programs.service_area}`}>
+        <div className="container">
+            <div className="grid grid-cols-4 gap-5">
+                <div>                
+                    <div className={`${programs.single_service} ${programs.lego_league_jr}`}>
+                        <div className={programs.service_icon}>
+                        </div>
+                        <div className={programs.service_content}>
+                            <h5>FIRST
+                                LEGO®
+                                League Jr.</h5>
+                            <p>Captures young curiosity by exploring real-world scientific challenges, learning teamwork, and working with motorized LEGO® elements.
+
+                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`${programs.single_service} ${programs.lego_league}`}>
+                        <div className={programs.service_icon}>
+                        </div>
+                        <div className={programs.service_content}>
+                            <h5>FIRST
+                                LEGO®
+                                League
+                                </h5>
+                            <p>Elementary and middle school-aged students research a real-world engineering challenge, develop a solution, and compete with LEGO-based robots of their own design </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`${programs.single_service} ${programs.ftc}`}>
+                        <div className={programs.service_icon}>
+                        </div>
+                        <div className={programs.service_content}>
+                            <h5>FIRST
+                                Tech
+                                Challenge</h5>
+                            <p>Teams of middle and high school-aged students are challenged to design, build, and program a robot to play a floor game against other teams’ creations.                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`${programs.single_service} ${programs.frc}`}>
+                        <div className={programs.service_icon}>
+                        </div>
+                        <div className={programs.service_content}>
+                            <h5>FIRST
+                                Robotics
+                                Competition</h5>
+                            <p>Teams of middle and high school-aged students are challenged to design, build, and program a robot to play a floor game against other teams’ creations.
+                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+
+    );
+};
+
+export default Programs;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Index from "../components/index";
-
+import Programs from "../components/programs";
 const App = () => {
     return (
         <div>
             <Index></Index>
+            <Programs></Programs>
         </div>
     )
 }
