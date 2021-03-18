a8652f09f294ef1427c7e58ec29e02a5acdb859d
NatashaSetiadi <natset456@gmail.com>
2021-03-18 10:03:02 -0400

    :sparkles: implemented banner component for the mission statement page

---
+++ components/MissionImpactPage/pageBanner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import pageBanner from "./pageBanner.module.css";
+
+let PageBanner = () => {
+  return (
+    <div>
+      <section className={`${pageBanner.home_banner_area} ${pageBanner.page_banner}`}>
+        <div className={`${pageBanner.banner_inner} ${pageBanner.page_banner}`}>
+          <div className={`${pageBanner.overlay}`}></div>
+
+          <div className="container banner_inner d-flex align-items-center">
+            <div className="row">
+              <div className="col-xl-12">
+                <div className={`${pageBanner.banner_content}`}>
+                  <h3 className="text-center">Mission Statement</h3>
+                  <p className="text-center">Impact the next generation of changemakers!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img className={`${pageBanner.arrow_object}`} src="/images/arrow-object-white.svg" alt="" />
+
+      </section>
+    </div>
+  );
+};
+
+export default PageBanner;
--- pages/mission-impact.js
+++ pages/mission-impact.js
@@ -3,11 +3,13 @@ import HomeAbout from "../components/HomePage/homeAbout";
 import Footer from "../components/footer";
 import Sponsors from "../components/sponsors";
 import Header from "../components/header";
+import PageBanner from "../components/MissionImpactPage/pageBanner";
 
 let MissionImpact = () => {
     return (
         <div>
             <Header></Header>
+            <PageBanner></PageBanner>
             <Sponsors></Sponsors>
             <Footer></Footer>
         </div>
